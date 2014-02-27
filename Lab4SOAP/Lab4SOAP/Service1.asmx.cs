16285c8c4e3ef9ee2433dad9deadb0661aeb1e5a
ajswdf <ajswdf@mail.umkc.edu>
2014-02-26 21:00:18 -0600

    Commit

---
+++ Lab4SOAP/Lab4SOAP/Service1.asmx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace Lab4SOAP
+{
+    /// <summary>
+    /// Summary description for Service1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Service1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string queryTable(string id)
+        {
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["dbString"].ConnectionString);
+
+            conn.Open();
+
+            string firstName = "";
+            string lastName = "";
+            SqlCommand cmd = new SqlCommand("Select * from table where id=" + id);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                firstName = firstName + ";" + reader["firstname"];
+                lastName = lastName + ";" + reader["lastname"];
+            }
+            reader.Close();
+            conn.Close();
+            return "info: " + firstName + ", " + lastName;
+        }
+    }
+}
---
+++ dbo.Table.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [First Name] VARCHAR(50) NULL, 
+    [Last Name] VARCHAR(50) NULL
+)
